ad27845bb177842987451cf3ad89d521329f712d
apachqq <work.tsyganov@mail.ru>
2022-05-18 22:30:07 +0300

    firstTime

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div class="addProductSelect">
+        <div>Добавление товара</div>
+        <div>
+            <select>
+                <option disabled value="">По умолчанию</option>
+                <option value=""></option>
+            </select>
+        </div>
+    </div>
 </template>
 
+<script>
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    .addProductSelect {
+        display: flex;
+        justify-content: space-between;
 
-nav {
-  padding: 30px;
+        div:nth-child(1) {
+            margin: 30px 0 16px 32px;
+            font-weight: 600;
+            font-size: 28px;
+            line-height: 35px;
+        }
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+        div:nth-child(2) {
+            margin: 32px 32px 0 0;
+        }
     }
-  }
-}
 </style>
