6b710b8ced48c2411562d87df436cdb915b97609
apachqq <work.tsyganov@mail.ru>
2022-05-19 20:27:40 +0300

    level-2

---
+++ src/components/MySelect.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="select">
+        <select>
+            <option disabled value="">По умолчанию</option>
+            <option value=""></option>
+        </select>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
