59401f69f74b8ac60e36a2601e61374f2537fd82
apachqq <work.tsyganov@mail.ru>
2022-05-20 12:12:21 +0300

    isValid-form-and-class

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -2,7 +2,7 @@
     <form>
         <div class="form-control">
             <label for="name">Наименование товара</label>
-            <input type="text" id="name" placeholder="Введите наименование товара" required>
+            <input type="text" id="name" placeholder="Введите наименование товара" required v-model.trim="name">
         </div>
         <div>
             <label for="description">Описание товара</label>
@@ -11,18 +11,37 @@
         </div>
         <div>
             <label for="url">Ссылка на изображение товара</label>
-            <input type="url" id="url" placeholder="Введите ссылку">
+            <input type="url" id="url" placeholder="Введите ссылку" required v-model.trim="url">
         </div>
         <div>
             <label for="price">Цена товара</label>
-            <input type="text" id="price" placeholder="Введите цену">
+            <input type="number" id="price" placeholder="Введите цену" required v-model.trim="price">
         </div>
-        <button class="btn" @click.prevent>Добавить товар</button>
+        <button class="btn" :class="{btnn : isValid}">Добавить товар</button>
     </form>
 </template>
 
 <script>
+    export default {
+        data() {
+            return {
+                name: '',
+                url: '',
+                price: ''
+            }
+        },
+        computed: {
+            isValid() {
+                return this.name !== '' && this.url !== '' && this.price !== ''
+            }
+        }
+    }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    .btnn {
+        background: #7BAE73;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        border-radius: 10px;
+    }
 </style>
