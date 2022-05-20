090067bb7989af4f9dbde706bc2dbfd4fd5ee7b3
apachqq <work.tsyganov@mail.ru>
2022-05-20 13:37:30 +0300

    animation-added-offer

--- src/components/OfferList.vue
+++ src/components/OfferList.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
         <div class="cards">
-            <card-item
-                    v-for="offer in offers"
-                    :key="offer.id"
-                    :offer="offer"
-            ></card-item>
+            <transition-group name="offer-list">
+                <card-item
+                        v-for="offer in offers"
+                        :key="offer.id"
+                        :offer="offer"
+                ></card-item>
+            </transition-group>
         </div>
     </div>
 </template>
@@ -24,4 +26,19 @@
 </script>
 
 <style scoped>
+    .offer-list-item {
+        display: inline-block;
+        margin-right: 10px;
+    }
+
+    .offer-list-enter-active,
+    .offer-list-leave-active {
+        transition: all 0.7s ease;
+    }
+
+    .offer-list-enter-from,
+    .offer-list-leave-to {
+        opacity: 0;
+        transform: translateY(130px);
+    }
 </style>
