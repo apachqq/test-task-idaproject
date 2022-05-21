490bc6fbe4ae17a985f5b60a0a1c2ebf63101d98
apachqq <work.tsyganov@mail.ru>
2022-05-21 15:35:09 +0300

    hover-img

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="card">
-        <img :src="offer.picture" width="332" height="200" />
-        <img class="delete-icon" src="img/delete.svg" @click="$emit('remove', offer)" />
+        <img class="scale" :src="offer.picture" width="332" height="200"/>
+        <img class="delete-icon" src="img/delete.svg" @click="$emit('remove', offer)"/>
         <div class="card-name">{{ offer.title }}</div>
         <div class="card-description">{{ offer.body }}</div>
         <div class="card-price">{{ offer.price }} руб.</div>
@@ -18,5 +18,14 @@
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    .scale {
+        display: block;
+        overflow: hidden;
+        transition: 1s;
+
+        &:hover {
+            transform: scale(1.2);
+        }
+    }
 </style>
--- src/components/MySelect.vue
+++ src/components/MySelect.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="select">
         <select @change="changeOption">
-            <option value="">По умолчанию</option>
+            <option>По умолчанию</option>
             <option
                     v-for="option in options"
                     :key="option.value"
                     :value="option.value"
-            >{{ option.name }}</option>
+            >
+                {{ option.name }}
+            </option>
         </select>
     </div>
 </template>
@@ -27,8 +29,17 @@
     }
 </script>
 
-<style scoped>
-    .select {
-
+<style lang="scss" scoped>
+    select {
+        background: #FFFEFB;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        border-radius: 4px;
+        border: none;
+        padding: 10px 17px 11px 16px;
+        appearance: none;
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 15px;
+        color: black;
     }
 </style>
